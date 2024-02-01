1bcdb16f77aed1ba443b480d767d4437eae74b73
mahdiramazani <mahdiramazani1281@gmail.com>
2024-02-01 18:05:25 +0330

    fix 4 bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,159 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Application extends StatefulWidget {
+  @override
+  State<Application> createState() => _ApplicationState();
+}
 
-  // This widget is the root of your application.
+class _ApplicationState extends State<Application> {
+  bool isturnO = true;
+  List<String> trunOorTrunX = ["", "", "", "", "", "", "", "", ""];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: _getAppBar(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _get_score_and_name_palyer("Player X", "2"),
+                  _get_score_and_name_palyer("Player O", "2"),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              _getRowBox(),
+              _getTurne(),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Widget _getTurne() {
+    return Text(
+      isturnO ? "Turne X" : "Turne O",
+      style: TextStyle(color: Colors.white, fontSize: 20.0),
+    );
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Widget _getRowBox() {
+    return Expanded(
+      child: GridView.builder(
+        itemCount: 9,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              getOnTap(index);
+            },
+            child: Container(
+              width: 10,
+              height: 10,
+              decoration: BoxDecoration(
+                border: Border.all(width: 0.5, color: Colors.grey),
+              ),
+              child: Center(
+                child: Text(
+                  "${trunOorTrunX[index]}",
+                  style: TextStyle(
+                      color:
+                          trunOorTrunX[index] == "X" ? Colors.red : Colors.blue,
+                      fontSize: 50),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _resetGame() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      for (var i = 0; i < trunOorTrunX.length; i++) {
+        if (trunOorTrunX[i] != "") {
+          trunOorTrunX[i] = "";
+        }
+      }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  void getOnTap(int index) {
+    setState(() {
+      if (isturnO && trunOorTrunX[index] == "") {
+        trunOorTrunX[index] = "X";
+        isturnO = !isturnO;
+      } else {
+        if (trunOorTrunX[index] == "") {
+          trunOorTrunX[index] = "O";
+          isturnO = !isturnO;
+        }
+      }
+    });
+  }
+
+  Widget _get_score_and_name_palyer(String player, String score) {
+    return Column(
+      children: [
+        Text(
+          player,
+          style: TextStyle(
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
         ),
+        SizedBox(height: 10),
+        Text(
+          score,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+          ),
+        ),
+      ],
+    );
+  }
+
+  PreferredSizeWidget _getAppBar() {
+    return AppBar(
+      backgroundColor: Colors.grey[900],
+      elevation: 0,
+      centerTitle: true,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "TicTacToe",
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(
+            width: 87,
+          ),
+          IconButton(
+            onPressed: () {
+              _resetGame();
+            },
+            icon: Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
